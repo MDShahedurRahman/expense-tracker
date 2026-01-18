ab1e771e8d4af2a8e7d0cc6aa7aac49228e19173
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-17 20:45:21 -0500

    add load_expenses method

--- file_handler.py
+++ file_handler.py
@@ -1,2 +1,10 @@
 import json
 from config import DATA_FILE
+
+
+def load_expenses():
+    try:
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
