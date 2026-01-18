40a1e4824444c4b1f73352aa7c7ec93e9926bc69
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-17 20:59:26 -0500

    add expenses_by_category report

--- report.py
+++ report.py
@@ -1,2 +1,9 @@
 def total_expenses(expenses):
     return sum(e["amount"] for e in expenses)
+
+
+def expenses_by_category(expenses):
+    report = {}
+    for e in expenses:
+        report[e["category"]] = report.get(e["category"], 0) + e["amount"]
+    return report
