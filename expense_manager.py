e18f9cc91770423a593523caf713efd4bc150fe6
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-17 20:54:02 -0500

    add add_expense method

--- expense_manager.py
+++ expense_manager.py
@@ -1,2 +1,9 @@
 from expense import Expense
 from utils import generate_id, validate_amount, validate_date
+
+
+def add_expense(expenses, title, amount, category, date):
+    expense_id = generate_id(expenses)
+    expense = Expense(expense_id, title, amount, category, date)
+    expenses.append(expense.to_dict())
+    return expenses
