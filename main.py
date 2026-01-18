fc730290c549a5fe250a97d43bbb16c677c1e80f
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-17 21:03:25 -0500

    save expenses to file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from report import total_expenses
 def main():
     expenses = load_expenses()
     expenses = add_expense(expenses, "Lunch", 12.5, "Food", "2026-01-15")
+    save_expenses(expenses)
 
 
 if __name__ == "__main__":
