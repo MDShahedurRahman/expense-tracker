1e6bc154fc056e7fcd8192634d59bd3290eb9647
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-17 21:03:00 -0500

    add sample expense

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from report import total_expenses
 
 def main():
     expenses = load_expenses()
+    expenses = add_expense(expenses, "Lunch", 12.5, "Food", "2026-01-15")
 
 
 if __name__ == "__main__":
